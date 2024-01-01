b5abdd81eeda6de39cb2a0173efe32e828f6d234
hnsia <44498178+hnsia@users.noreply.github.com>
2024-01-01 23:07:35 +0800

    feat: create base user resolver

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register user
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    return user;
+  }
+
+  // login user
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password,
+    };
+
+    return user;
+  }
+
+  // get all users
+  async getUsers() {
+    const users = [];
+
+    return users;
   }
 }
