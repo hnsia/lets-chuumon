aade065a6f598b1cc4454afbc36e96e48a327e44
hnsia <44498178+hnsia@users.noreply.github.com>
2024-01-04 23:06:03 +0800

    feat: setup user creation

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto, RegisterDto } from './dto/user.dto';
 import { PrismaService } from '@/prisma/Prisma.service';
+import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
@@ -13,15 +14,27 @@ export class UsersService {
   ) {}
 
   // register user
-  async register(registerDto: RegisterDto) {
+  async register(registerDto: RegisterDto, response: Response) {
     const { name, email, password } = registerDto;
-    const user = {
-      name,
-      email,
-      password,
-    };
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-    return user;
+    if (isEmailExist) {
+      throw new BadRequestException('User with this email already exists!');
+    }
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return { user, response };
   }
 
   // login user
