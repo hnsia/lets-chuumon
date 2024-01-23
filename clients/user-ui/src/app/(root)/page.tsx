c27276fb7ed3268d407e01999e527fea09ac2749
hnsia <44498178+hnsia@users.noreply.github.com>
2024-01-23 22:08:49 +0800

    feat: setup nextUI

--- clients/user-ui/src/app/(root)/page.tsx
+++ clients/user-ui/src/app/(root)/page.tsx
@@ -1,7 +1,12 @@
+import HomeScreen from "@/src/screens/HomeScreen";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <HomeScreen />
+    </div>
+  );
 };
 
 export default Page;
