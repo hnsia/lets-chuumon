b2abd8b84588da1b93d97f28bb5a9df0c4f9fcb6
hnsia <44498178+hnsia@users.noreply.github.com>
2024-01-24 13:09:13 +0800

    feat: setting up themes

--- clients/user-ui/src/components/Layout/Header.tsx
+++ clients/user-ui/src/components/Layout/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
+    <header className="w-full h-[80px] bg-[#0F1524] flex items-center justify-between">
       <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
     </header>
   );
