07c408330be6b63f7e5f2edbd00e36659eb1d156
hnsia <44498178+hnsia@users.noreply.github.com>
2024-02-06 22:51:54 +0800

    fix: color issues

--- clients/user-ui/src/components/Layout/Header.tsx
+++ clients/user-ui/src/components/Layout/Header.tsx
@@ -1,10 +1,12 @@
-import { Avatar } from "@nextui-org/avatar";
+import styles from "@/src/utils/style";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="w-full h-[80px] bg-[#0F1524] flex items-center justify-between">
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+      <div className="w-[90%] m-auto text-white">
+        <h1 className={`${styles.logo}`}>Let&apos;s Chuumon</h1>
+      </div>
     </header>
   );
 };
